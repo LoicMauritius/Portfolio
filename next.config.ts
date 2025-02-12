c689a0a9beba355f4004a419633cfd0dc091f30e
LoicMauritius <loicmauritius@gmail.com>
2025-02-12 19:13:29 +0100

    Remove the base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/portfolio",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
