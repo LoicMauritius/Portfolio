2b1db249db1b1d4f73a5b64d738dc42169964295
LoicMauritius <loicmauritius@gmail.com>
2025-02-16 15:28:39 +0100

    Asemble all components to form the main web page

--- src/app/web/page.tsx
+++ src/app/web/page.tsx
@@ -1,17 +1,25 @@
 "use client";
 
-import { Header, MainTitle } from "@/components";
+import { Header, MainTitle, ProjectsList } from "@/components";
 import { useLanguageDetection } from "@/hook";
 import { mainStyle } from "@/styles";
+import { ProjectDisplayerProps } from "@/components";
 
 export default function Web() {
 
     const { t } = useLanguageDetection();
+
+    console.log("Type de t('projects.web'):", typeof t("projects.web"), t("projects.web"));
+    console.log("projects.web: ", t("projects.web"))
+
+    const projects = t("projects.web", { returnObjects: true }) as ProjectDisplayerProps[];
+
     return (
         <>
             <Header />
             <main style={mainStyle}>
                 <MainTitle title={t("titres.web")} />
+                <ProjectsList projects={projects} />
             </main>
         </>
     );
