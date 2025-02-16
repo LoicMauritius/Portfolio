310064c9c73f75fb5c1d3fbfd7dd880ee34a2de6
LoicMauritius <loicmauritius@gmail.com>
2025-02-16 17:52:08 +0100

    Add project separators

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -4,6 +4,7 @@ import { Header, MainTitle, ProjectDisplayerProps } from "@/components";
 import { MobileProjectDisplayer } from "@/components";
 import { useLanguageDetection } from "@/hook";
 import { mainStyle, ProjectsListStyle } from "@/styles";
+import Image from "next/image";
 
 export default function Mobile() {
     const { t } = useLanguageDetection();
@@ -16,13 +17,26 @@ export default function Mobile() {
             <main style={mainStyle}>
                 <MainTitle title={t("titres.mobile")} />
                 <div style={ProjectsListStyle}>
-                    {projects.map((project) => <MobileProjectDisplayer 
+                    {projects.map((project,index) => 
+                    <>
+                        <MobileProjectDisplayer 
                         key={`mobile-${project.title}`}
                         title={project.title} 
                         description={project.description} 
                         technologies={project.technologies} 
                         fonctionnalitees_principale={project.fonctionnalitees_principale} 
-                        images={project.images} />)}
+                        images={project.images} />
+                        <Image
+                            key={`seperator-project-${project.title}-${index}`}
+                            src={"/projectSeparator.png"}
+                            alt={"projectSeparator"}
+                            width={0}
+                            height={230} 
+                            sizes="100vw"
+                            style={{ objectFit: "fill", width: "100vw", maxHeight: "230px" }}
+                        />
+                    </>
+                    )}
                 </div>
             </main>
         </>
